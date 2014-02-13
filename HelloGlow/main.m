a831bcd040db5c3c8fdca60f88703299e0b54200
Bossbazen <f.baas@student.fontys.nl>
2014-02-13 11:49:30 +0100

    Initial Commit

---
+++ HelloGlow/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloGlow
+//
+//  Created by Frank Baas on 13-02-14.
+//  Copyright (c) 2014 Frank Baas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
